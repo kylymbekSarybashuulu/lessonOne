8b9f40d6e0c464648e2f7bd314fcdb57e7100faf
KYLYMBEK <kylymbek344@gmail.com>
2022-09-07 19:57:48 +0300

    birinchi commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,11 @@
+package com.company;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Sallam Java3");
+
+        Studentjava studentjava = new Studentjava("Timur",20);
+        System.out.println(studentjava);
     }
 }
